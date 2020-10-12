379e7d414fc1622851c834a417fc301cc48b8c3c
J. van der Kruit <justin.van.der.kruit@hva.nl>
2020-10-12 15:26:18 +0200

    Added Spring

---
+++ backend/src/main/java/nl/infosupport2/zonneveld/ZonneveldApplication.java
@@ -0,0 +1,13 @@
+package nl.infosupport2.zonneveld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonneveldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZonneveldApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/nl/infosupport2/zonneveld/ZonneveldApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.infosupport2.zonneveld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZonneveldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
