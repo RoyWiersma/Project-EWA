84934beb21cec65619570f92e19aaab5f3cdea92
D.S. Nansink <danny.nansink@hva.nl>
2020-10-14 13:46:17 +0200

    home component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SidenavComponent} from './sidenav/sidenav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
     declarations: [
-        SidenavComponent
+        SidenavComponent,
+        HomeComponent
     ],
     exports: [
         SidenavComponent
