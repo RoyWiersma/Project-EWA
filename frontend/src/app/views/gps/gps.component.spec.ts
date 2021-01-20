d2feff6d25bf0ae9c91406125e5d024e32ee53ef
Roy <roy.wiersma@hva.nl>
2021-01-20 15:11:25 +0100

    Added Test to gps component

--- frontend/src/app/views/gps/gps.component.spec.ts
+++ frontend/src/app/views/gps/gps.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GpsComponent } from './gps.component';
+import {GpService} from '../../services/gp.service';
+import {GpMockService} from '../../mocks/gp-mock.service';
+import {DebugElement} from '@angular/core';
+
 
 describe('GpsComponent', () => {
   let component: GpsComponent;
@@ -8,7 +12,14 @@ describe('GpsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GpsComponent ]
+      declarations: [ GpsComponent ],
+
+        providers: [
+            {
+                provide: GpService,
+                useClass: GpMockService,
+            }
+        ]
     })
     .compileComponents();
   }));
@@ -22,4 +33,21 @@ describe('GpsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render 10 items in the table', () => {
+        const dbElement: DebugElement = fixture.debugElement;
+        const tableRows: HTMLTableRowElement[] = dbElement.nativeElement.querySelectorAll('tbody tr');
+
+        expect(tableRows.length).toEqual(10);
+  });
+  it('should open the modal', async(() => {
+        spyOn(component, 'openModal');
+
+        const button = fixture.debugElement.nativeElement.querySelector('button');
+        button.click();
+
+        fixture.whenStable().then(() => {
+            expect(component.openModal).toHaveBeenCalled();
+        });
+    }));
 });
