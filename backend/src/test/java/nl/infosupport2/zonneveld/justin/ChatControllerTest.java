b7084db61bbbdad93ae9b41be9d797ce623a38fd
J. van der Kruit <justin.van.der.kruit@hva.nl>
2021-01-21 13:05:03 +0100

    Finished test

--- backend/src/test/java/nl/infosupport2/zonneveld/justin/ChatControllerTest.java
+++ backend/src/test/java/nl/infosupport2/zonneveld/justin/ChatControllerTest.java
@@ -1,6 +1,5 @@
 package nl.infosupport2.zonneveld.justin;
 
-import nl.infosupport2.zonneveld.TokenManager;
 import nl.infosupport2.zonneveld.entities.Chat;
 import nl.infosupport2.zonneveld.entities.GP;
 import nl.infosupport2.zonneveld.entities.Patient;
@@ -12,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,12 +18,11 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -39,28 +36,20 @@ public class ChatControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
     @MockBean
     private ChatRepository chatRepository;
-
-    @Autowired
-    private JacksonTester<Chat> jsonChat;
 
     private Patient testPatient;
     private GP testDoctor;
 
     @BeforeEach
     void setup() {
-        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-
         testDoctor = new GP();
         testPatient = new Patient();
     }
 
     @Test
-    void gets401WhenTryingToGetChatsByDoctor() throws Exception {
+    void gets403WhenTryingToGetChatsByDoctorWithoutAuthentication() throws Exception {
         given(chatRepository.findByDoctor(testDoctor))
                 .willReturn(List.of(new Chat("test-test-123", testDoctor, testPatient)));
 
@@ -69,22 +58,19 @@ public class ChatControllerTest {
                         .accept(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse();
 
-        assertEquals(HttpStatus.UNAUTHORIZED.value(), response.getStatus());
+        assertEquals(HttpStatus.FORBIDDEN.value(), response.getStatus());
     }
 
     @Test
-    void gets200WhenTryingToGetChatsByDoctor() throws Exception {
-        String token = TokenManager.generateToken("test@test.nl");
-
+    void gets403WhenTryingToDeleteChatWithoutAuthentication() throws Exception {
         given(chatRepository.findByDoctor(testDoctor))
                 .willReturn(List.of(new Chat("test-test-123", testDoctor, testPatient)));
 
         MockHttpServletResponse response = mvc.perform(
-                get("/chat")
-                        .header("Authorization", token)
-                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                delete("/chat/test-test-123")
+                        .accept(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse();
 
-        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals(HttpStatus.FORBIDDEN.value(), response.getStatus());
     }
 }
